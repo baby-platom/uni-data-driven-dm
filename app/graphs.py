6d4b55162a411222dac541d31b40a04e62ca8da0
baby-platom <platon.programmer@gmail.com>
2025-03-16 11:01:19 +0100

    Update: add graphs create funcs

---
+++ app/graphs.py
@@ -0,0 +1,29 @@
+"""Functions to create reference model graphs."""
+
+import networkx as nx
+
+
+def create_er_graph(nodes: int, edges: int) -> nx.Graph:
+    p: float = (2 * edges) / (nodes * (nodes - 1)) if nodes > 1 else 0
+    return nx.erdos_renyi_graph(nodes, p)
+
+
+def create_ba_graph(nodes: int, edges: int) -> nx.Graph:
+    _edges: int = max(1, round(edges / nodes))
+    if nodes <= _edges:
+        raise ValueError("Can not create BA graph")
+    return nx.barabasi_albert_graph(nodes, _edges)
+
+
+def create_ws_graph(nodes: int, edges: int) -> nx.Graph:
+    neighbours: int = round(2 * edges / nodes)
+    if neighbours % 2 != 0:
+        neighbours += 1
+
+    neighbours = max(2, neighbours)
+    if neighbours >= nodes:
+        neighbours = nodes - 1 if (nodes - 1) % 2 == 0 else nodes - 2
+
+    if nodes <= neighbours:
+        raise ValueError("Can not create WS graph")
+    return nx.watts_strogatz_graph(nodes, neighbours, 0.1)
